4e15dca0370b8c26a5f9b0dff983db94a90a9eae
Matias K <matiaskelman.com>
2025-04-11 10:39:40 -0300

    Arreglando myapp

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,12 @@
+var createError = require('http-errors');
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
 var indexRouter = require('./routes/index');
-var loginRouter = require('./routes/login');
+var usersRouter = require('./routes/users');
+
 var app = express();
 
 // view engine setup
@@ -14,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', loginRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ myapp/db/db.js
@@ -0,0 +1,4 @@
+let usuario = {
+    usuario: {id: 1, email: "a@gmail.com", contraseña: "12345678!", fecha: "1/1/3", fotoPerfil: ""},
+    posts: []
+}
