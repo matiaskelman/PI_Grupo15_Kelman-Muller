c301a95015236ceb6bf8671729fe2491f4102475
Matias K <matiaskelman.com>
2025-04-11 11:21:34 -0300

    punto 2 listo

--- myapp/db/db.js
+++ myapp/db/db.js
@@ -1,4 +1,14 @@
-let usuario = {
-    usuario: {id: 1, email: "a@gmail.com", contraseña: "12345678!", fecha: "1/1/3", fotoPerfil: ""},
-    posts: []
+let db = {
+    usuario: { id: 1, email: "a@gmail.com", contraseña: "12345678!", fecha: "1/1/3", fotoPerfil: "" },
+    posts: [
+    { id: 1, img: "img1.png", nombre: "Plancha", descripcion: "Descripcion 1", user: 1, comentarios: [{ user: 1, texto: "Texto comentario" }, { user: 2, texto: "Texto comentario 2" }, { user: 1, texto: "Texto comentario 3" }] },
+    { id: 2, img: "img1.png", nombre: "Jarra", descripcion: "Descripcion 2", user: 1, comentarios: [{ user: 1, texto: "Texto comentario" }, { user: 2, texto: "Texto comentario 2" }, { user: 1, texto: "Texto comentario 3" }] },
+    { id: 3, img: "img1.png", nombre: "Plastico", descripcion: "Descripcion 3", user: 1, comentarios: [{ user: 1, texto: "Texto comentario" }, { user: 2, texto: "Texto comentario 2" }, { user: 1, texto: "Texto comentario 3" }] },
+    { id: 4, img: "img1.png", nombre: "Goma", descripcion: "Descripcion 5", user: 1, comentarios: [{ user: 1, texto: "Texto comentario" }, { user: 2, texto: "Texto comentario 2" }, { user: 1, texto: "Texto comentario 3" }] },
+    { id: 5, img: "img1.png", nombre: "Mouse", descripcion: "Descripcion 6", user: 1, comentarios: [{ user: 1, texto: "Texto comentario" }, { user: 2, texto: "Texto comentario 2" }, { user: 1, texto: "Texto comentario 3" }] },
+    { id: 6, img: "img1.png", nombre: "Computadora", descripcion: "Descripcion 7", user: 1, comentarios: [{ user: 1, texto: "Texto comentario" }, { user: 2, texto: "Texto comentario 2" }, { user: 1, texto: "Texto comentario 3" }] },
+    { id: 7, img: "img1.png", nombre: "Licuadora", descripcion: "Descripcion 8", user: 1, comentarios: [{ user: 1, texto: "Texto comentario" }, { user: 2, texto: "Texto comentario 2" }, { user: 1, texto: "Texto comentario 3" }] },
+    { id: 8, img: "img1.png", nombre: "Teclado", descripcion: "Descripcion 9", user: 1, comentarios: [{ user: 1, texto: "Texto comentario" }, { user: 2, texto: "Texto comentario 2" }, { user: 1, texto: "Texto comentario 3" }] },
+    { id: 9, img: "img1.png", nombre: "usb", descripcion: "Descripcion 10", user: 1, comentarios: [{ user: 1, texto: "Texto comentario" }, { user: 2, texto: "Texto comentario 2" }, { user: 1, texto: "Texto comentario 3" }] },
+    { id: 10, img: "img1.png", nombre: "usb-c", descripcion: "Descripcion 4", user: 1, comentarios: [{ user: 1, texto: "Texto comentario" }, { user: 2, texto: "Texto comentario 2" }, { user: 1, texto: "Texto comentario 3" }] }]
 }
