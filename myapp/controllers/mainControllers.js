17de2b86cdd4ee819749c5a5c1641d61756dd2ad
Matias K <matiaskelman.com>
2025-05-21 12:50:20 -0300

    avanzando 2da entrega

--- myapp/controllers/mainControllers.js
+++ myapp/controllers/mainControllers.js
@@ -1,9 +1,9 @@
 const db = require('../db/db'); // Asegurate de que db.js tenga `module.exports = db;`
-
+const logueado = require("../db/logueado")
 module.exports = {
     index: function(req, res) {
         let productos = db.posts; // 'posts' es la propiedad que tiene los productos
-        res.render('index', { productos }); // Pasás los productos a la vista
+        res.render('index', { productos: productos, logueado: logueado }); // Pasás los productos a la vista
     },
 
     login: function(req, res) {
