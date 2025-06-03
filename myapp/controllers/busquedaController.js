312f0d89cc501add22e97b8950e58b440ab78d0a
Matias K <matiaskelman.com>
2025-06-03 00:07:32 -0300

    Anda la busqueda (llamada con rafa)

--- myapp/controllers/busquedaController.js
+++ myapp/controllers/busquedaController.js
@@ -1,14 +1,22 @@
-const db = require('../db/db');
+const db = require("../database/models"); // Asegurate de que db.js tenga `module.exports = db;`
+const logueado = require("../db/logueado");
+const op = db.Sequelize.Op;
+const Productos = db.Product
 const busquedaControllers =  {
     busqueda: function(req, res) {
       const requerimiento = req.query.search;
       
-      const resultados = [
+  Productos.findAll(
+   {    where: [
+                    {  nombre: {[op.like]: `%${requerimiento}%` } }
+                ]}
+  ).then(function (resultados) {
+    console.log(resultados)
+return  res.render('resultados', { resultados, resultados});
+  })
+    
   
-
-      ];
-  
-      res.render('resultados', { resultados, requerimiento});
+     
     }
   };
   
